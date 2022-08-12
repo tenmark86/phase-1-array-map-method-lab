7a7893c870e6d805ccfc77adffe0227ab9670a44
tenmark86e <tenmark86@gmail.com>
2022-08-12 16:06:23 +0300

    Done Using the Array Map Method assignment

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+let tutorialsCapitalized = [];
+
 const titleCased = () => {
-  return tutorials
-}
+  tutorials.map(function titleCased(stringVariable){
+
+    let newArr = [];
+   
+       function stringToArray(string){
+         newArr = string.split(" ");
+       };
+       stringToArray(stringVariable);
+   
+   
+       const capitalized = newArr.map( function(string) {
+         return string.charAt(0).toUpperCase()+string.slice(1);
+   })
+   
+       const modifiedString = capitalized.join(" ");
+       tutorialsCapitalized.push(modifiedString);
+       console.log(modifiedString);
+ 
+ });
+   return tutorialsCapitalized;
+ } 
+
